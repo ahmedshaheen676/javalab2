5db40916911030222114c992bdd4d7cda3e68755
ahmedshaheen <ahmedshaheen676@yahoo.com>
2019-12-24 15:41:04 +0200

    lab2 commit

--- com/shaheen/Main.java
+++ com/shaheen/Main.java
@@ -1,6 +1,6 @@
 package com.shaheen;
 public class lab1{
 	public static void main(String arg[]){
-		System.out.println("welecome to our program");
+		System.out.println("welecome to our prog from github");
 	}
 }
